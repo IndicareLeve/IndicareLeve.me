0d3a9ae1c34d80623c6b152b1a011b9c3bd42488
IndicareLeve <indicareleve@gmail.com>
2022-01-21 14:38:29 +0100

    work in progress

--- src/App.svelte
+++ src/App.svelte
@@ -2,29 +2,19 @@
 	export let name: string;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="grid">
+	<div class="header">
+		<h1>IndicareLeve.</h1>
+		<h6>aka Daniele Cervi</h6>
+	</div>
+	<div class="intro">
+		<p class="intro">Geek, developer, motorcyclist, mech keyboard lover. Not necessarily in this order.</p>
+	</div>
+	<div class="under-construction">
+		<p class="under-construction">🚧 This is under construction! 🚧</p>
+	</div>
+	<div class="work">
+		<p class="collaborate">If you'd like to collaborate on websites, integrations or anything code-related please <a href="mailto:indicareleve@gmail.com">drop me a line</a>!</p>
+		<p class="work">If you are a recruiter, you might want to <a href="https://raw.githubusercontent.com/IndicareLeve/resume/master/resume.pdf">take a look at my cv</a>.</p>
+	</div>
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
